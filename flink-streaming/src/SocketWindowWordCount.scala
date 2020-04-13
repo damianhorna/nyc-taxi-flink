a33430edb107b28ee4c293698a12e83d3b701cf8
damian-horna <damian.horna@interia.pl>
2020-04-13 15:22:26 +0200

    Lookup borough name and print

--- flink-streaming/src/SocketWindowWordCount.scala
+++ flink-streaming/src/SocketWindowWordCount.scala
@@ -2,54 +2,97 @@ import java.util.Properties
 
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.flink.api.common.restartstrategy.RestartStrategies
 
 import scala.collection.mutable
+import org.apache.flink.streaming.api.watermark.Watermark
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks
+import scala.math.max
 
-/**
- * Implements a streaming windowed version of the "WordCount" program.
- *
- * This program connects to a server socket and reads strings from the socket.
- * The easiest way to try this out is to open a text sever (at port 12345)
- * using the ''netcat'' tool via
- * {{{
- * nc -l 12345 on Linux or nc -l -p 12345 on Windows
- * }}}
- * and run this example with the hostname and the port as arguments..
- */
+import java.text.SimpleDateFormat
+
+class BoundedOutOfOrdernessGenerator extends AssignerWithPeriodicWatermarks[TripEvent] {
+  def getTimestamp(s: String) : Long = {
+    val format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    format.parse(s).getTime
+  }
+
+  val maxOutOfOrderness = 6000L // 6 seconds
+  var currentMaxTimestamp: Long = _
+  override def extractTimestamp(te: TripEvent, previousElementTimestamp: Long): Long = {
+    val timestamp = getTimestamp(te.timestamp)
+    currentMaxTimestamp = max(timestamp, currentMaxTimestamp)
+    timestamp
+  }
+  override def getCurrentWatermark: Watermark = {
+    new Watermark(currentMaxTimestamp - maxOutOfOrderness)
+  }
+}
+
 object SocketWindowWordCount {
 
   def read_csv(): mutable.Map[String, (_, _, _)] = {
-    var lookup = mutable.Map[String, (_, _, _)]()
+    var boroughLookup = mutable.Map[String, (_, _, _)]()
 
     println("Reading csv file")
     val bufferedSource = io.Source.fromFile("/home/dhorna/dev/studies/mgr-sem1/bd/nyc-taxi-flink/data/taxi_zone_lookup.csv")
     for (line <- bufferedSource.getLines) {
       val Array(locationId, borough, zone, serviceZone)  = line.drop(1).split(",").map(_.trim)
-      lookup += (locationId -> (borough, zone, serviceZone))
+      boroughLookup += (locationId -> (borough, zone, serviceZone))
     }
     bufferedSource.close
-    lookup
+    boroughLookup
   }
 
   /** Main program method */
   def main(args: Array[String]) : Unit = {
 
-    var lookup = read_csv()
+    val lookup : mutable.Map[String, (_, _, _)] = read_csv()
+    val no_of_retries = 3
 
     // get the execution environment
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    env.getConfig.setRestartStrategy(RestartStrategies.fixedDelayRestart(no_of_retries, 0))
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
     val properties = new Properties()
     properties.setProperty("bootstrap.servers", "localhost:9092")
     properties.setProperty("group.id", "testGroup")
 
-    var text : DataStream[String] = env
+    val text: DataStream[String] = env
       .addSource(new FlinkKafkaConsumer[String]("testTopic", new SimpleStringSchema(), properties))
 
-    text.rebalance.map(s => s).print().setParallelism(1)
+//    text.rebalance.map(s => s).print().setParallelism(1)
+
+    val tripEventsDS: org.apache.flink.streaming.api.scala.DataStream[TripEvent] =
+      text.filter(s => !s.startsWith("event_type")).
+        map(_.split(",")).
+        filter(_.length == 9).
+        filter(arr => lookup.keySet.contains(arr(3))).
+        map(arr => TripEvent(
+          arr(0).toInt,
+          arr(1).toInt,
+          arr(2),
+          arr(3).toInt,
+          lookup(arr(3))._1.toString,
+          arr(4).toInt,
+          arr(5).toDouble,
+          arr(6).toInt,
+          arr(7).toDouble,
+          arr(8).toInt
+        ))
+
+    val wTaWTripEventsDS : DataStream[TripEvent] = tripEventsDS.
+      assignTimestampsAndWatermarks(new BoundedOutOfOrdernessGenerator())
+
+    wTaWTripEventsDS.map(te => te.borough).print().setParallelism(1)
+
+
+
 
     env.execute("Socket Window WordCount")
   }
---
+++ flink-streaming/src/TripEvent.scala
@@ -0,0 +1,11 @@
+case class TripEvent (
+                       trip_id: Integer,
+                       start_stop: Integer,
+                       timestamp: String,
+                       location_id: Integer,
+                       borough: String,
+                       passenger_count: Integer,
+                       trip_distance: Double,
+                       payment_type: Integer,
+                       amount: Double,
+                       vendor_id: Integer)
