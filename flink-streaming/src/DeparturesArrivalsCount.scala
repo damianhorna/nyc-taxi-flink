654ec7a146d8f1ac15420c793207afd871cc4c69
damian-horna <damian.horna@interia.pl>
2020-04-14 14:18:44 +0200

    Extracted some logic

--- flink-streaming/src/DeparturesArrivalsCount.scala
+++ flink-streaming/src/DeparturesArrivalsCount.scala
@@ -10,11 +10,11 @@ import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.assigners.{SlidingEventTimeWindows, TumblingEventTimeWindows}
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.elasticsearch.{ElasticsearchSinkFunction, RequestIndexer}
-import org.apache.flink.streaming.connectors.elasticsearch7.{ElasticsearchSink, RestClientFactory}
+import org.apache.flink.streaming.connectors.elasticsearch7.ElasticsearchSink
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
 import org.apache.http.HttpHost
 import org.elasticsearch.action.index.IndexRequest
-import org.elasticsearch.client.{Requests, RestClientBuilder}
+import org.elasticsearch.client.Requests
 import utils.CsvReader
 
 import scala.collection.mutable
@@ -69,7 +69,7 @@ object DeparturesArrivalsCount {
       keyBy(mar => mar.borough + mar.day).
       process(new StatefulAccumulationKeyedProcessFun)
 
-//        finalDS.print().setParallelism(1)
+    finalDS.print().setParallelism(1)
 
     val anomalyDS: DataStream[AnomalyAggResult] = wTaWTripEventsDS.
       keyBy(te => te.borough + te.day).
@@ -80,37 +80,20 @@ object DeparturesArrivalsCount {
       process(new AnomalyProcessWindowFun).
       filter(aag => aag.diff > L.toInt)
 
-//    anomalyDS.print().setParallelism(1)
+    //    anomalyDS.print().setParallelism(1)
 
     val httpHosts = new java.util.ArrayList[HttpHost]
     httpHosts.add(new HttpHost("127.0.0.1", 9200, "http"))
 
     val esSinkBuilder = new ElasticsearchSink.Builder[DeparturesArrivalsAggResult](
       httpHosts,
-      new ElasticsearchSinkFunction[DeparturesArrivalsAggResult] {
-        def process(element: DeparturesArrivalsAggResult, ctx: RuntimeContext, indexer: RequestIndexer) {
-          val json = new java.util.HashMap[String, Any]
-          json.put("hour", element.hour)
-          json.put("borough", element.borough)
-          json.put("day", element.day)
-          json.put("arrivals_count",element.arrivalsCnt)
-          json.put("departures_count", element.departuresCnt)
-          json.put("arriving_people_count", element.arrivingPeopleCnt)
-          json.put("departing_people_count", element.departingPeopleCnt)
-
-          val rqst: IndexRequest = Requests.indexRequest
-            .index("nyc-index")
-            .source(json)
-
-          indexer.add(rqst)
-        }
-      }
+      new ElasticSystemImageSink
     )
 
     esSinkBuilder.setBulkFlushMaxActions(1)
 
     // finally, build and add the sink to the job's pipeline
-//    finalDS.addSink(esSinkBuilder.build)
+    //    finalDS.addSink(esSinkBuilder.build)
 
 
     env.execute("Socket Window WordCount")
@@ -119,4 +102,23 @@ object DeparturesArrivalsCount {
   /** Data type for words with count */
   case class WordWithCount(word: String, count: Long)
 
+}
+
+class ElasticSystemImageSink extends ElasticsearchSinkFunction[DeparturesArrivalsAggResult] {
+  def process(element: DeparturesArrivalsAggResult, ctx: RuntimeContext, indexer: RequestIndexer) {
+    val json = new java.util.HashMap[String, Any]
+    json.put("hour", element.hour)
+    json.put("borough", element.borough)
+    json.put("day", element.day)
+    json.put("arrivals_count", element.arrivalsCnt)
+    json.put("departures_count", element.departuresCnt)
+    json.put("arriving_people_count", element.arrivingPeopleCnt)
+    json.put("departing_people_count", element.departingPeopleCnt)
+
+    val rqst: IndexRequest = Requests.indexRequest
+      .index("nyc-index")
+      .source(json)
+
+    indexer.add(rqst)
+  }
 }
