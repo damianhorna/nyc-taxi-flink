dcc51cc3f454cf6fb3c57921e25fae7d512a8e6f
damian-horna <damian.horna@interia.pl>
2020-04-14 14:07:17 +0200

    Add elasticsearch sink

--- flink-streaming/src/DeparturesArrivalsCount.scala
+++ flink-streaming/src/DeparturesArrivalsCount.scala
@@ -2,13 +2,19 @@ import java.text.SimpleDateFormat
 import java.util.Properties
 
 import datatypes.{AnomalyAggResult, DeparturesArrivalsAggResult, TripEvent}
+import org.apache.flink.api.common.functions.RuntimeContext
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.assigners.{SlidingEventTimeWindows, TumblingEventTimeWindows}
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.connectors.elasticsearch.{ElasticsearchSinkFunction, RequestIndexer}
+import org.apache.flink.streaming.connectors.elasticsearch7.{ElasticsearchSink, RestClientFactory}
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.http.HttpHost
+import org.elasticsearch.action.index.IndexRequest
+import org.elasticsearch.client.{Requests, RestClientBuilder}
 import utils.CsvReader
 
 import scala.collection.mutable
@@ -74,7 +80,37 @@ object DeparturesArrivalsCount {
       process(new AnomalyProcessWindowFun).
       filter(aag => aag.diff > L.toInt)
 
-    anomalyDS.print().setParallelism(1)
+//    anomalyDS.print().setParallelism(1)
+
+    val httpHosts = new java.util.ArrayList[HttpHost]
+    httpHosts.add(new HttpHost("127.0.0.1", 9200, "http"))
+
+    val esSinkBuilder = new ElasticsearchSink.Builder[DeparturesArrivalsAggResult](
+      httpHosts,
+      new ElasticsearchSinkFunction[DeparturesArrivalsAggResult] {
+        def process(element: DeparturesArrivalsAggResult, ctx: RuntimeContext, indexer: RequestIndexer) {
+          val json = new java.util.HashMap[String, Any]
+          json.put("hour", element.hour)
+          json.put("borough", element.borough)
+          json.put("day", element.day)
+          json.put("arrivals_count",element.arrivalsCnt)
+          json.put("departures_count", element.departuresCnt)
+          json.put("arriving_people_count", element.arrivingPeopleCnt)
+          json.put("departing_people_count", element.departingPeopleCnt)
+
+          val rqst: IndexRequest = Requests.indexRequest
+            .index("nyc-index")
+            .source(json)
+
+          indexer.add(rqst)
+        }
+      }
+    )
+
+    esSinkBuilder.setBulkFlushMaxActions(1)
+
+    // finally, build and add the sink to the job's pipeline
+//    finalDS.addSink(esSinkBuilder.build)
 
 
     env.execute("Socket Window WordCount")
