8114dcb5980f41dfdd7fccdb08e7f3a2234bff43
damian-horna <damian.horna@interia.pl>
2020-04-14 16:46:00 +0200

    Add borough to anomaly

--- flink-streaming/src/sink/ElasticAnomalySink.scala
+++ flink-streaming/src/sink/ElasticAnomalySink.scala
@@ -13,6 +13,7 @@ class ElasticAnomalySink extends ElasticsearchSinkFunction[AnomalyAggResult] {
     val json = new java.util.HashMap[String, Any]
     json.put("start", new SimpleDateFormat("yyyy-MM-dd H").parse(element.start))
     json.put("stop", new SimpleDateFormat("yyyy-MM-dd H").parse(element.stop))
+    json.put("borough", element.borough)
     json.put("arriving_people_count", element.arrivingPeopleCnt)
     json.put("departing_people_count", element.departingPeopleCnt)
     json.put("diff", element.diff)
