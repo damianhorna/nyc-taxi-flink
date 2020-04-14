43b42e753565948bd0b90e3bb51b8f4f5e215509
damian-horna <damian.horna@interia.pl>
2020-04-14 02:28:22 +0200

    Aggregating on a daily basis

--- flink-streaming/src/DeparturesArrivalsCount.scala
+++ flink-streaming/src/DeparturesArrivalsCount.scala
@@ -18,32 +18,46 @@ import scala.collection.mutable
 
 class MyProcessFunction extends KeyedProcessFunction[String, MyAggResult, MyAggResult] {
 
-  private var acc: ValueState[Int] = _
+  private var g_arrivals_count: ValueState[Int] = _
+  private var g_departures_count: ValueState[Int] = _
+  private var g_arriving_ppl_count: ValueState[Int] = _
+  private var g_departing_ppl_count: ValueState[Int] = _
 
   override def processElement(i: MyAggResult, context: KeyedProcessFunction[String, MyAggResult, MyAggResult]#Context, collector: Collector[MyAggResult]): Unit = {
-    var tmpAcc = 0
-    try{
-      tmpAcc = acc.value
-    } catch {
-      case x: NullPointerException => tmpAcc = 0
-    }
 
-    val newAcc = tmpAcc + i.arrivals_count.toInt
-    acc.update(newAcc)
-    collector.collect(new MyAggResult(
+    val new_arrivals_count = g_arrivals_count.value + i.arrivals_count.toInt
+    val new_departures_count = g_departures_count.value + i.departures_count.toInt
+    val new_arriving_ppl_count = g_arriving_ppl_count.value + i.arriving_ppl_count.toInt
+    val new_departing_ppl_count = g_departing_ppl_count.value + i.departing_ppl_count.toInt
+
+    g_arrivals_count.update(new_arrivals_count)
+    g_departures_count.update(new_departures_count)
+    g_arriving_ppl_count.update(new_arriving_ppl_count)
+    g_departing_ppl_count.update(new_departing_ppl_count)
+
+    collector.collect(MyAggResult(
       i.hour,
       i.borough,
       i.day,
-      newAcc,
-      i.departures_count,
-      i.arriving_ppl_count,
-      i.departing_ppl_count
+      new_arrivals_count,
+      new_departures_count,
+      new_arriving_ppl_count,
+      new_departing_ppl_count
     ))
   }
 
   override def open(parameters: Configuration): Unit = {
-    acc = getRuntimeContext.getState(
-      new ValueStateDescriptor[(Int)]("acc", createTypeInformation[(Int)])
+    g_arrivals_count = getRuntimeContext.getState(
+      new ValueStateDescriptor[(Int)]("g_arrivals_count", createTypeInformation[(Int)])
+    )
+    g_departures_count = getRuntimeContext.getState(
+      new ValueStateDescriptor[(Int)]("g_departures_count", createTypeInformation[(Int)])
+    )
+    g_arriving_ppl_count = getRuntimeContext.getState(
+      new ValueStateDescriptor[(Int)]("g_arriving_ppl_count", createTypeInformation[(Int)])
+    )
+    g_departing_ppl_count = getRuntimeContext.getState(
+      new ValueStateDescriptor[(Int)]("g_departing_ppl_count", createTypeInformation[(Int)])
     )
   }
 }
